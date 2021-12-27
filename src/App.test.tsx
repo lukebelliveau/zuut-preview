ae34bc3ab5321eb4de04830a4869dae26323da8f
Erica Kastner <dkastner@gmail.com>
2021-12-27 11:09:45 -0600

    [ZPM-29] Add eslint-a11y, axe accessibility unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { axe, toHaveNoViolations } from 'jest-axe';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
+expect.extend(toHaveNoViolations);
 
 test('renders learn react link', () => {
   const { getByText } = render(
@@ -12,4 +14,15 @@ test('renders learn react link', () => {
   );
 
   expect(getByText(/learn/i)).toBeInTheDocument();
+});
+
+test('is accessible', async () => {
+  const { container } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const results = await axe(container);
+
+  expect(results).toHaveNoViolations();
 });
