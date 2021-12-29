08bd3e235968be4ddab5d4aec61e5dad5f6bfeb3
Erica Kastner <dkastner@gmail.com>
2021-12-28 20:46:25 -0600

    Fix specs

--- src/routes/Home.test.tsx
+++ src/routes/Home.test.tsx
@@ -2,24 +2,24 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { axe, toHaveNoViolations } from 'jest-axe';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import { store } from '../app/store';
+import Home from './Home';
 expect.extend(toHaveNoViolations);
 
 test('renders learn react link', () => {
   const { getByText } = render(
     <Provider store={store}>
-      <App />
+      <Home />
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/Welcome/i)).toBeInTheDocument();
 });
 
 test('is accessible', async () => {
   const { container } = render(
     <Provider store={store}>
-      <App />
+      <Home />
     </Provider>
   );
   const results = await axe(container);
