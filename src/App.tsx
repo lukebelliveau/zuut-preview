d62f7a2ccaf11434e0c52682239c513f3bd4fd30
Erica Kastner <dkastner@gmail.com>
2021-12-28 22:19:17 -0600

    [ZPM-34] Beginning of basic app layout - header

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './routes/Home';
 import NotFound from './routes/NotFound';
+import Workplace from './routes/Workplace';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/workplace" element={<Workplace />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
---
+++ src/components/Layout.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Layout.css';
+
+type LayoutProps = {
+  children: React.ReactNode;
+  header: String;
+};
+
+export default function Layout(props: LayoutProps) {
+  return (
+    <div className="wrapper">
+      <header>
+        <img id="logo" src="/logo.png" alt="GreenGrids Logo" />
+        <h1>GreenGrids</h1>
+      </header>
+      <nav>
+        <ul>
+          <li><Link to="/playground">Playground</Link></li>
+          <li><Link to="/shopping">Shopping List</Link></li>
+          <li><Link to="/workplace">Workplace</Link></li>
+        </ul>
+      </nav>
+      <div className="content">
+        <h2>{props.header}</h2>
+        {props.children}
+      </div>
+    </div>
+  );
+}
