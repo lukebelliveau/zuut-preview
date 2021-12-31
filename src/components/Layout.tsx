61b857a3da93266c034e011755da2ccd8ff7c507
Erica Kastner <dkastner@gmail.com>
2021-12-31 14:17:29 -0600

    [ZPM-34] Workspace layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -16,25 +16,34 @@ export default function Layout(props: LayoutProps) {
         <img id="logo" src="/logo.png" alt="GreenGrids Logo" />
         <h1>GreenGrids</h1>
       </header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/playground">Playground</Link>
-          </li>
-          <li>
-            <Link to="/shopping">Shopping List</Link>
-          </li>
-          <li>
-            <Link to="/workplace">Workplace</Link>
-          </li>
-          <li>
-            <LogoutButton />
-          </li>
-        </ul>
-      </nav>
-      <div className="content">
-        <h2>{props.header}</h2>
-        {props.children}
+      <div id="sidebar">
+        <div id="dashboard-accordian">
+          Dashboard
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/playground">Playground</Link>
+            </li>
+            <li>
+              <Link to="/shopping">Shopping List</Link>
+            </li>
+            <li>
+              <Link to="/workplace">Workplace</Link>
+            </li>
+            <li>
+              <LogoutButton />
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="content">
+        <div id="subheading">
+          <h2>{props.header}</h2>
+        </div>
+        <div id="main">
+          {props.children}
+        </div>
       </div>
     </div>
   );
