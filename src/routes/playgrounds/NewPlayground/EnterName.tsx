3f490107ffc15a4dcf562f762af82149ce3394fe
Erica Kastner <dkastner@gmail.com>
2022-01-20 21:17:31 -0600

    [ZPM-1] Save name input without form submission

--- src/routes/playgrounds/NewPlayground/EnterName.tsx
+++ src/routes/playgrounds/NewPlayground/EnterName.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import React, { ChangeEventHandler, KeyboardEventHandler } from 'react';
+import { ChangeHandler, SubmitHandler, useForm } from 'react-hook-form';
 
 import Section from './Section';
 import NextButton from './NextButton';
@@ -18,27 +18,29 @@ type EnterNameProps = {
 const planRepo = PlanRepository.forRedux();
 
 export default function EnterName(props: EnterNameProps) {
-  const { register, handleSubmit } = useForm<PlanNameParams>();
-
-  const onSubmit: SubmitHandler<PlanNameParams> = data => {
+  const onChange: ChangeEventHandler<HTMLInputElement> = event => {
     const plan = planRepo.default();
-    plan.name = data.name;
+    plan.name = event.target.value;
     planRepo.update(plan);
-    props.nextPage();
+  };
+  const onKeyDown: KeyboardEventHandler<HTMLInputElement> = event => {
+    if (event.key === 'Enter') {
+      props.nextPage();
+    }
   };
 
   const plan = planRepo.default();
 
   return (<>
     <Section>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <h2>
-          <label htmlFor="name">Give your grow a name.</label>
-        </h2>
-        <input type="text" placeholder="type the name here"
-          value={plan?.name}
-          {...register('name', { required: true })} />
-      </form>
+      <h2>
+        <label htmlFor="name">Give your grow a name.</label>
+      </h2>
+      <input type="text" placeholder="type the name here"
+        value={plan?.name}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+         />
     </Section>
     <NextButton nextPage={props.nextPage} />
   </>);
