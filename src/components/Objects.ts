cff476bab4a58ddd9e5fc97f6bccf4bcf4edd41d
Erica Kastner <dkastner@gmail.com>
2022-01-27 22:31:47 -0600

    [ZPM-7] Add all the misc shopping list items

--- src/components/Objects.ts
+++ src/components/Objects.ts
@@ -17,7 +17,21 @@ const ObjectsMenu: IItemGroup[] = [
   {
     itemGroup: 'Misc',
     items: [
-      new MiscItem('Nutrients')
+      new MiscItem('Chip Bag Clips'),
+      new MiscItem('CO2 System'),
+      new MiscItem('Extension Cords'),
+      new MiscItem('Eye Protection'),
+      new MiscItem('Measuring Spoons'),
+      new MiscItem('Nutrients'),
+      new MiscItem('PH adjuster'),
+      new MiscItem('PH test'),
+      new MiscItem('Plant Twist Tie'),
+      new MiscItem('Plant yoyos'),
+      new MiscItem('Rope Ratchets'),
+      new MiscItem('Surge Protectors'),
+      new MiscItem('Syringes'),
+      new MiscItem('Timer'),
+      new MiscItem('Zip Ties'),
     ]
   },
 ];
