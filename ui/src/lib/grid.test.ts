3ce40c4ed183c86dfca7c13dbd61e523a83cd300
luke <luke.belliveau@gmail.com>
2022-02-11 11:27:42 -0500

    change default grid interval to be 0.5ft

--- ui/src/lib/grid.test.ts
+++ ui/src/lib/grid.test.ts
@@ -1,9 +1,10 @@
+import { feetToMm } from './conversions';
 import Grid from './grid';
 
 describe('Grid', () => {
   describe('#lines', () => {
     it('returns a set of lines for a room', () => {
-      const grid = new Grid(609.6, 609.6);
+      const grid = new Grid(609.6, 609.6, feetToMm(1));
       expect(grid.lines).toEqual([
         [0, 0, 0, 609.6],
         [304.8, 0, 304.8, 609.6],
@@ -15,43 +16,59 @@ describe('Grid', () => {
 
   describe('#snapPostition', () => {
     it('returns the same position for an item aligned along a grid line', () => {
-      const grid = new Grid(609.6, 609.6);
-      expect(grid.snapPostition({ x: 304.8, y: 304.8 })).toEqual({ x: 304.8, y: 304.8 });
+      const grid = new Grid(609.6, 609.6, feetToMm(1));
+      expect(grid.snapPostition({ x: 304.8, y: 304.8 })).toEqual({
+        x: 304.8,
+        y: 304.8,
+      });
     });
 
     it('returns the closest position for an item closer to an X gridline on the left', () => {
-      const grid = new Grid(609.6, 609.6);
-      expect(grid.snapPostition({ x: 314.8, y: 304.8 })).toEqual({ x: 304.8, y: 304.8 });
+      const grid = new Grid(609.6, 609.6, feetToMm(1));
+      expect(grid.snapPostition({ x: 314.8, y: 304.8 })).toEqual({
+        x: 304.8,
+        y: 304.8,
+      });
     });
 
     it('returns the closest position for an item closer to an X gridline on the right', () => {
-      const grid = new Grid(609.6, 609.6);
-      expect(grid.snapPostition({ x: 280.8, y: 304.8 })).toEqual({ x: 304.8, y: 304.8 });
+      const grid = new Grid(609.6, 609.6, feetToMm(1));
+      expect(grid.snapPostition({ x: 280.8, y: 304.8 })).toEqual({
+        x: 304.8,
+        y: 304.8,
+      });
     });
 
     it('returns the closest position for an item closer to a Y gridline on the top', () => {
-      const grid = new Grid(609.6, 609.6);
-      expect(grid.snapPostition({ x: 304.8, y: 284.8 })).toEqual({ x: 304.8, y: 304.8 });
+      const grid = new Grid(609.6, 609.6, feetToMm(1));
+      expect(grid.snapPostition({ x: 304.8, y: 284.8 })).toEqual({
+        x: 304.8,
+        y: 304.8,
+      });
     });
 
     it('returns the closest position for an item closer to a Y gridline on the bototm', () => {
-      const grid = new Grid(609.6, 609.6);
-      expect(grid.snapPostition({ x: 304.8, y: 344.8 })).toEqual({ x: 304.8, y: 304.8 });
+      const grid = new Grid(609.6, 609.6, feetToMm(1));
+      expect(grid.snapPostition({ x: 304.8, y: 344.8 })).toEqual({
+        x: 304.8,
+        y: 304.8,
+      });
     });
 
     it('returns the same position for an item equidistant from two X gridlines', () => {
-      const grid = new Grid(609.6, 609.6);
-      expect(grid.snapPostition({ x: 152.4, y: 304.8 })).toEqual({ x: 304.8, y: 304.8 });
+      const grid = new Grid(609.6, 609.6, feetToMm(1));
+      expect(grid.snapPostition({ x: 152.4, y: 304.8 })).toEqual({
+        x: 304.8,
+        y: 304.8,
+      });
     });
 
     it('returns the same position for an item equidistant from two Y gridlines', () => {
-      const grid = new Grid(609.6, 609.6);
-      expect(grid.snapPostition({ x: 304.8, y: 152.4 })).toEqual({ x: 304.8, y: 304.8 });
+      const grid = new Grid(609.6, 609.6, feetToMm(1));
+      expect(grid.snapPostition({ x: 304.8, y: 152.4 })).toEqual({
+        x: 304.8,
+        y: 304.8,
+      });
     });
-
-    // it("doesn't allow an item to go off the grid", () => {
-    //   const grid = new Grid(609.6, 609.6);
-    //   expect(grid.snapPostition({ x: 599, y: 599 })).toEqual({ x: 304.8, y: 304.8 });
-    // });
   });
 });
--- ui/src/lib/grid.ts
+++ ui/src/lib/grid.ts
@@ -1,35 +1,45 @@
+import { feetToMm } from './conversions';
 import { Point } from './point';
-
-const SNAP_INTERVAL = 304.8; // 12in
 
 export default class Grid {
   width: number;
   length: number;
+  snapInterval: number;
 
-  constructor(width: number, length: number) {
+  constructor(
+    width: number,
+    length: number,
+    snapInterval: number = feetToMm(0.5)
+  ) {
     this.width = width;
     this.length = length;
+    this.snapInterval = snapInterval;
   }
 
   snapPostition(position: Point): Point {
     return {
-      x: Math.round(position.x / SNAP_INTERVAL) * SNAP_INTERVAL,
-      y: Math.round(position.y / SNAP_INTERVAL) * SNAP_INTERVAL,
+      x: Math.round(position.x / this.snapInterval) * this.snapInterval,
+      y: Math.round(position.y / this.snapInterval) * this.snapInterval,
     };
   }
 
   get verticalLines(): number[][] {
     const lines = [];
-    for (let i = 0; i < this.width / SNAP_INTERVAL; i++) {
-      lines.push([i * SNAP_INTERVAL, 0, i * SNAP_INTERVAL, this.length]);
+    for (let i = 0; i < this.width / this.snapInterval; i++) {
+      lines.push([
+        i * this.snapInterval,
+        0,
+        i * this.snapInterval,
+        this.length,
+      ]);
     }
     return lines;
   }
 
   get horizontalLines(): number[][] {
     const lines = [];
-    for (let i = 0; i < this.length / SNAP_INTERVAL; i++) {
-      lines.push([0, i * SNAP_INTERVAL, this.width, i * SNAP_INTERVAL]);
+    for (let i = 0; i < this.length / this.snapInterval; i++) {
+      lines.push([0, i * this.snapInterval, this.width, i * this.snapInterval]);
     }
     return lines;
   }
