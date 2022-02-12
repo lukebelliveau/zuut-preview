015b7ec51031a7e4d32424f1173e6451ef19123d
Erica Kastner <dkastner@gmail.com>
2022-02-12 16:20:26 -0600

    [ZPM-51] Add jwt Apollo context

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,10 @@
 import { ApolloServer } from 'apollo-server-express';
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import express from 'express';
-import http from 'http';
 import proxy from 'express-http-proxy';
-import { readFileSync } from 'fs';
+import jwt from 'express-jwt';
+import { existsSync, readFileSync } from 'fs';
+import http from 'http';
 
 import { typeDefs } from './typedefs';
 import { resolvers } from './resolvers';
@@ -12,16 +13,25 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 const PORT = parseInt(process.env.PORT || '4000');
 const UI_BUILD_DIR = `${__dirname}/../../ui/build`;
 
-const indexHtml = readFileSync(`${UI_BUILD_DIR}/index.html`);
+const indexHtmlPath = `${UI_BUILD_DIR}/index.html`;
+const indexHtml = existsSync(indexHtmlPath) ? readFileSync(indexHtmlPath) : '';
 
 async function listen(port: number) {
   const app = express();
   const httpServer = http.createServer(app);
 
+  app.use('/graphql', jwt({
+    secret: process.env.AUTH0_SECRET || '',
+    algorithms: ['HS256']
+  }));
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    context: ({ req }) => {
+      return { user: req.user };  // req.user comes from express-jwt
+    },
   });
   await server.start();
 
@@ -35,12 +45,12 @@ async function listen(port: number) {
     const proxyUrl = `http://localhost:${(PORT + 100)}`;
     console.log(`Proxying web requests to ${proxyUrl}`)
     app.use('/', proxy(proxyUrl));
+  } else {
+    app.use('/*', (_, res) => {
+      res.setHeader('content-type', 'text/html; charset=UTF-8');
+      res.send(indexHtml);
+    });
   }
-
-  app.use('/*', (_, res) => {
-    res.setHeader('content-type', 'text/html; charset=UTF-8');
-    res.send(indexHtml);
-  });
 
   return new Promise((resolve, reject) => {
     httpServer.listen(port).once('listening', resolve).once('error', reject);
