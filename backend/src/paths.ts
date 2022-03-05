f4d0d8b2e6468936b6a51171575418e563ac74e9
Erica Kastner <dkastner@gmail.com>
2022-03-04 22:48:31 -0600

    Fix static content path

--- backend/src/paths.ts
+++ backend/src/paths.ts
@@ -1,4 +1,4 @@
-export const GRAPHQL_TYPES = __dirname.includes('dist') ? 
-  `${__dirname}/../../../graphql/types.graphql` :
-  `${__dirname}/../../graphql/types.graphql`;
-export const UI_BUILD_DIR = `${__dirname}/../../ui/build`;
+const appRoot = __dirname.includes('dist') ? `${__dirname}/../../..` : `${__dirname}/../..`;
+
+export const GRAPHQL_TYPES = `${appRoot}/graphql/types.graphql`;
+export const UI_BUILD_DIR = `${appRoot}/ui/build`;
