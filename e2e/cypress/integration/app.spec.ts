49dbb735f6f12c424ad2dda1bb15864b21303ffe
luke <luke.belliveau@gmail.com>
2022-04-11 20:51:42 -0600

    fix e2e test

--- e2e/cypress/integration/app.spec.ts
+++ e2e/cypress/integration/app.spec.ts
@@ -12,7 +12,7 @@ describe('app', () => {
      * this block is testing Auth0 UI code which makes the test more brittle
      * but unfortunately there is no way to programmatically log in a user
      * with `useAuth0`. The only known hack involves using localStorage
-     * for the JWT but this is a higher security risk as it exposes the 
+     * for the JWT but this is a higher security risk as it exposes the
      * token details to the user.
      * https://github.com/auth0/auth0-react/issues/234
      */
@@ -28,14 +28,13 @@ describe('app', () => {
 
     cy.findByText('Name: Test Grow').should('exist');
 
-
     cy.findByText('Objects').click();
 
     // add item
     cy.findByRole('button', { name: /Pot 2x2/i }).click();
 
-    // select item from Inventory
-    cy.findByRole('menuitem', { name: /Pot 2x2/i }).click();
+    // // select item from Inventory
+    // cy.findByRole('menuitem', { name: /Pot 2x2/i }).click();
 
     // see control panel
     cy.findByText('Description').should('exist');
