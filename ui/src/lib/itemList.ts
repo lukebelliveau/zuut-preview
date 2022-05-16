061a1324f2df1b018d49d6f044a3bc293b1e9168
luke <luke.belliveau@gmail.com>
2022-05-16 17:33:22 -0400

    sort growspaces under all other items

--- ui/src/lib/itemList.ts
+++ ui/src/lib/itemList.ts
@@ -1,7 +1,17 @@
 import { LayerState } from '../features/interactions/interactionsState';
 import { IItem } from './item';
+import { isGrowspace } from './item/growspace';
 import { IPlaceableItem, isPlaceableItem } from './item/placeableItem';
 import { Layer } from './layer';
+
+const sortGrowspaceToBottom = (a: IItem, b: IItem) => {
+  if (isGrowspace(a) && !isGrowspace(b)) {
+    return -1;
+  } else if (isGrowspace(b) && !isGrowspace(a)) {
+    return 1;
+  }
+  return 0;
+};
 
 /**
  * If an item is selected, place it at the end of the array so Konva renders it on top.
@@ -12,6 +22,8 @@ export const sortSelectedToLast = (
 ): IItem[] => {
   const itemsCopy = [...items];
   const sortedItems = itemsCopy.sort((a, b) => {
+    if (isGrowspace(a) || isGrowspace(b)) return sortGrowspaceToBottom(a, b);
+
     if (selectedItemIds.includes(a.id) && !selectedItemIds.includes(b.id)) {
       return 1;
     }
@@ -28,21 +40,23 @@ export const sortSelectedToLast = (
   return itemList;
 };
 
-export const sortCeilingItemsToLast = (items: (IItem | IPlaceableItem)[]) => {
+export const sortFromGroundToCeiling = (items: (IItem | IPlaceableItem)[]) => {
   const itemsCopy = [...items];
   const sortedItems = itemsCopy.sort((a, b) => {
     if (isPlaceableItem(a) && !isPlaceableItem(b)) {
       return 1;
     } else if (isPlaceableItem(b) && !isPlaceableItem(a)) {
+      console.log('????');
       return -1;
     } else if (!isPlaceableItem(a) && !isPlaceableItem(b)) {
       return 0;
     } else if (isPlaceableItem(a) && isPlaceableItem(b)) {
+      if (isGrowspace(a) || isGrowspace(b)) return sortGrowspaceToBottom(a, b);
+
+      // sort Floor items under Ceiling items
       if (a.layer === Layer.FLOOR && b.layer !== Layer.FLOOR) {
         return -1;
-      }
-
-      if (a.layer !== Layer.FLOOR && b.layer === Layer.FLOOR) {
+      } else if (a.layer !== Layer.FLOOR && b.layer === Layer.FLOOR) {
         return 1;
       }
     }
@@ -61,7 +75,7 @@ export const sortItems = (
   showLayer: LayerState
 ): IItem[] => {
   // ceiling items should display on top, so sort to end of array
-  let sortedItems = sortCeilingItemsToLast(items);
+  let sortedItems = sortFromGroundToCeiling(items);
 
   // ... unless the ceiling layer is disabled
   if (showLayer[Layer.FLOOR] === true && showLayer[Layer.CEILING] === false) {
