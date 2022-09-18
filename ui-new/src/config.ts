a09a2ade4f4e5e4b9465f1005e1e4f07dc14b914
luke <luke.belliveau@gmail.com>
2022-09-18 22:09:57 +0200

    change default layout to vertical

--- ui-new/src/config.ts
+++ ui-new/src/config.ts
@@ -75,7 +75,7 @@ export const defaultSettings: SettingsValueProps = {
   themeMode: 'light',
   themeDirection: 'ltr',
   themeContrast: 'default',
-  themeLayout: 'horizontal',
+  themeLayout: 'vertical',
   themeColorPresets: 'default',
   themeStretch: false,
 };
