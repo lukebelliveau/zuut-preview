b0d1e59cc175c3ad6aab1038c869b38e6ece40db
luke <luke.belliveau@gmail.com>
2022-11-03 16:47:59 +0100

    fix LogoOnlyLayout spacing

--- ui-new/src/layouts/LogoOnlyLayout.tsx
+++ ui-new/src/layouts/LogoOnlyLayout.tsx
@@ -1,31 +1,12 @@
 import { Outlet } from 'react-router-dom';
 // @mui
 import { styled } from '@mui/material/styles';
-// components
-import Logo from '../components/Logo';
-
-// ----------------------------------------------------------------------
-
-const HeaderStyle = styled('header')(({ theme }) => ({
-  top: 0,
-  left: 0,
-  lineHeight: 0,
-  width: '100%',
-  position: 'absolute',
-  padding: theme.spacing(3, 3, 0),
-  [theme.breakpoints.up('sm')]: {
-    padding: theme.spacing(5, 5, 0)
-  }
-}));
-
-// ----------------------------------------------------------------------
+import MainHeader from './main/MainHeader';
 
 export default function LogoOnlyLayout() {
   return (
     <>
-      <HeaderStyle>
-        <Logo />
-      </HeaderStyle>
+      <MainHeader />
       <Outlet />
     </>
   );
